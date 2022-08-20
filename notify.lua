0b3b76746980b275813b046ce0a596a67528f0c0
5nz <94724852+5nz@users.noreply.github.com>
2022-08-19 23:08:02 -0600

    Update notify.lua

--- notify.lua
+++ notify.lua
@@ -1,13 +1,3 @@
--- local function callback(Text2)
---     if Text2 == Btn1 then
---      print("fuck me in the ass")
---     elseif Text2 == Btn2 then
---         print("guess you wont be fucking me today")
---     end
---  end
---  local NotificationBindable = Instance.new("BindableFunction")
---  NotificationBindable.OnInvoke = callback
-
 local Notification = {}
 
 function Notification:notify(Title, Text, Image, Duration)
@@ -20,5 +10,3 @@ function Notification:notify(Title, Text, Image, Duration)
     end
 
 local Add = Notification
-
-Add:notify("Hello", "Fuck You", 10100928853, 10)
